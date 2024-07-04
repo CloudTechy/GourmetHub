6c5b12af1a050033f1dd970af4335b1e0b9bca55
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-07-05 00:21:05 +0100

    test

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
